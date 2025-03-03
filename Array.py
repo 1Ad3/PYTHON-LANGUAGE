097a691a0c84e60ec8a87049fede41e44aa8692f
Allah dad <allahdad.bscsf22@iba-suk.edu.pk>
2025-03-03 12:03:01 +0500

    code

---
+++ Array.py
@@ -0,0 +1,34 @@
+from array import *
+temps = array ('i',[1,2,3])
+
+#for i in temps:
+#       print (temps)
+
+#for i in range (3):
+#       print (temps[i])
+
+for i in range (len(temps)):
+       print (temps[i])
+
+##temps.append (4)
+##print (temps)
+##temps.insert (0,0)
+##print (temps)
+##temps.remove (0)
+##print (temps)
+##temps.pop ()
+##print (temps)
+
+print (temps.index (1))
+temps.reverse ()
+print (temps.count (2))
+print (temps.buffer_info())
+
+
+
+
+#slice array
+num_list=[1,2,3,4,5,6,7,8]
+number_array=array('i',num_list)
+print("numbers are: " , number_array[1:7])
+
